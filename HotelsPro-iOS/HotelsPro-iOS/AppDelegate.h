7fe9153a9ec8ae38f234394f181fb45178b5012f
GoKuLkRiShNaN_R <gklkrshnn13@gmail.com>
2015-01-31 14:34:03 +0530

    "Project Creation"

---
+++ HotelsPro-iOS/HotelsPro-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HotelsPro-iOS
+//
+//  Created by Gokul Krishnan on 1/31/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HotelsPro-iOS/HotelsPro-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelsPro-iOS
+//
+//  Created by Gokul Krishnan on 1/31/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
