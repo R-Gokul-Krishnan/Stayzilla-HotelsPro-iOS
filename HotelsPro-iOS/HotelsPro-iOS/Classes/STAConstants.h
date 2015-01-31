48e90bb8bd63abb972317d20bf286c06b0ddb432
Vijayaragavan_M <vijayaragavan.manoharan@photoninfotech.net>
2015-02-01 05:15:26 +0530

    service class added

---
+++ HotelsPro-iOS/HotelsPro-iOS/Classes/STAConstants.h
@@ -0,0 +1,13 @@
+//
+//  STAConstants.h
+//  HotelsPro-iOS
+//
+//  Created by Vijayaragavan_M on 01/02/15.
+//
+//
+
+
+#define ERROR_NO_INTERNET_CONNECTION            @"No network connection available. Please check your internet connection."
+#define ERROR_GLOBAL_EXCEPTION                  @"Sorry! Something went wrong. Please try again after some time."
+#define kFailedLocationServiceEnable            @"Turn ON location services to allow Hotel-Pro app to determine your location."
+
