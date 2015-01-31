23709da596d805821617aaf0311706ccf6468acd
GoKuLkRiShNaN_R <gklkrshnn13@gmail.com>
2015-02-01 03:56:09 +0530

    #UPDATIONS :: RoomDetailsCell Updation

--- HotelsPro-iOS/HotelsPro-iOS/Classes/RoomsTableViewCell.h
+++ HotelsPro-iOS/HotelsPro-iOS/Classes/RoomsTableViewCell.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Paul Kalapati. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface RoomsTableViewCell : UITableViewCell
 
--- HotelsPro-iOS/HotelsPro-iOS/Classes/ViewControllers/iPhone/RoomDetailsViewController.h
+++ HotelsPro-iOS/HotelsPro-iOS/Classes/ViewControllers/iPhone/RoomDetailsViewController.h
@@ -6,20 +6,15 @@
 //  Copyright (c) 2015 Paul Kalapati. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface RoomDetailsViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UILabel *roomTypeValueLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *actualTarrifLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *discountedTarrifLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *numberOfOccupantsLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *extraBedValueLabel;
-
 @property (weak, nonatomic) IBOutlet UILabel *ichRoomValueLabel;
 
 @end
